d97a3317f9bcffe2da9ffd3f893d669ceb0859be
Alexeyzem <Alex.zem2015@mail.ru>
2024-09-24 18:33:23 +0300

    delete mock

--- internal/controller/router.go
+++ internal/controller/router.go
@@ -10,12 +10,6 @@ func NewAuthRouter(controller *AuthController) http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/auth/register", controller.Register)
 	mux.HandleFunc("/auth/login", controller.Auth)
-	mux.HandleFunc("/profile", MockHandler)
 	res := middleware.Auth(controller.sessionManager, mux)
 	return res
-}
-
-func MockHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Don't work auth"))
 }
