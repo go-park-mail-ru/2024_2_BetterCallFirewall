7f2cef013beca91986c03a8d909e940d6117160a
Alexeyzem <Alex.zem2015@mail.ru>
2024-09-28 21:29:15 +0300

    add answer to OPTIONS

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -19,6 +19,17 @@ type SessionManager interface {
 
 func Auth(sm SessionManager, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, application/json:charset=UTF-8")
+			w.Header().Set("Content-Type", "application/json:charset=UTF-8")
+
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		if _, ok := noAuthUrls[r.URL.Path]; ok {
 			next.ServeHTTP(w, r)
 			return
