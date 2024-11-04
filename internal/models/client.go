9858401a421093387adca84ae947685cd8f3f116
slashlight <savvateev_andreyka@mail.ru>
2024-11-04 23:20:42 +0300

    feat: added websockets

---
+++ internal/models/client.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"github.com/gorilla/websocket"
+
+	"github.com/2024_2_BetterCallFirewall/internal/chat/controller"
+)
+
+type Client struct {
+	Socket         *websocket.Conn
+	Receive        chan []byte
+	chatController *controller.ChatController
+}
+
+func (c *Client) Read() {
+	defer c.Socket.Close()
+	for {
+		_, jsonMessage, err := c.Socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.chatController.Messages <- jsonMessage
+	}
+}
+
+func (c *Client) Write() {
+	defer c.Socket.Close()
+	for msg := range c.Receive {
+		err := c.Socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
