7ec4753157f67f6ca1b78fdbe5696349a1ddbed2
slashlight <savvateev_andreyka@mail.ru>
2024-11-05 23:05:58 +0300

    bugfix: fixed consts

--- internal/chat/repository/postgres/queryConst.go
+++ internal/chat/repository/postgres/queryConst.go
@@ -34,14 +34,14 @@ FROM
 WHERE
     rn = 1 AND last_messages.created_at < $2
 ORDER BY
-    last_messages.created_at ASC
+    last_messages.created_at DESC
 LIMIT 15;`
 
 	getLatestMessagesBatch = `SELECT sender, receiver, content, created_at
 FROM message
 WHERE ((sender = $1 AND receiver = $2) OR (sender = $2 AND receiver = $1)) 
 AND created_at < $3
-ORDER BY created_at 
+ORDER BY created_at
 LIMIT 20;`
 
 	sendNewMessage = `INSERT INTO message(receiver, sender, content) VALUES ($1, $2, $3)`
