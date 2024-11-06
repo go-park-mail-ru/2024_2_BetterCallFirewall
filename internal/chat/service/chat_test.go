8daa65562ac8545342beab1c2677708f1d53e467
Alexeyzem <Alex.zem2015@mail.ru>
2024-11-06 17:59:32 +0300

    add test getallChat

--- internal/chat/service/chat_test.go
+++ internal/chat/service/chat_test.go
@@ -2,15 +2,23 @@ package service
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/2024_2_BetterCallFirewall/internal/models"
 )
 
+var errMock = errors.New("mock error")
+
 type MockRepo struct{}
 
 func (m MockRepo) GetChats(ctx context.Context, userID uint32, lastUpdateTime time.Time) ([]*models.Chat, error) {
+	if userID == 0 {
+		return nil, errMock
+	}
 	return []*models.Chat{}, nil
 }
 
@@ -31,5 +39,24 @@ type TestStructGetAllChats struct {
 
 func TestGetAllChats(t *testing.T) {
 	chatServ := NewChatService(MockRepo{})
+	tests := []TestStructGetAllChats{
+		{
+			userID:    0,
+			wantChats: nil,
+			wantErr:   errMock,
+		},
+		{
+			userID:    1,
+			wantChats: []*models.Chat{},
+			wantErr:   nil,
+		},
+	}
 
+	for _, tt := range tests {
+		res, err := chatServ.GetAllChats(context.Background(), tt.userID, tt.lastUpdateTime)
+		assert.Equal(t, tt.wantChats, res)
+		if !errors.Is(err, tt.wantErr) {
+			t.Errorf("GetAllChats() error = %v, wantErr %v", err, tt.wantErr)
+		}
+	}
 }
