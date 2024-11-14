bfca125fb7ed35bf3012f22f3eaee0b69e9537ee
slashlight <savvateev_andreyka@mail.ru>
2024-11-14 15:13:54 +0300

    feat: created struct for http metrics

---
+++ internal/metrics/http.go
@@ -0,0 +1,63 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type HttpMetrics struct {
+	Errors      *prometheus.CounterVec
+	serviceName string
+	Hits        *prometheus.CounterVec
+	Timings     *prometheus.HistogramVec
+}
+
+func NewHTTPMetrics(serviceName string) (*HttpMetrics, error) {
+	var metrics HttpMetrics
+	metrics.Errors = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "errors_total",
+			Help: "Number of total errors.",
+		},
+		[]string{"path", "service", "status"},
+	)
+	if err := prometheus.Register(metrics.Errors); err != nil {
+		return nil, err
+	}
+
+	metrics.Hits = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "hits_total",
+			Help: "Number of total hits.",
+		},
+		[]string{"path", "service", "status"},
+	)
+	if err := prometheus.Register(metrics.Hits); err != nil {
+		return nil, err
+	}
+
+	metrics.Timings = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "total_timings",
+		},
+		[]string{"path", "status"},
+	)
+	if err := prometheus.Register(metrics.Timings); err != nil {
+		return nil, err
+	}
+
+	metrics.serviceName = serviceName
+
+	return &metrics, nil
+}
+
+func (m *HttpMetrics) IncErrors(path string, status string) {
+	m.Errors.WithLabelValues(path, m.serviceName, status).Inc()
+}
+
+func (m *HttpMetrics) IncHits(path string, status string) {
+	m.Hits.WithLabelValues(path, m.serviceName, status).Inc()
+}
+
+func (m *HttpMetrics) ObserveTiming(path string, status string, time float64) {
+	m.Timings.WithLabelValues(path, status).Observe(time)
+}
