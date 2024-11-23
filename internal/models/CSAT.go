18ab1c85ac1e83ea8ffea1e7ee1357a5f753e0bd
slashlight <savvateev_andreyka@mail.ru>
2024-11-23 13:53:14 +0300

    feat: added json for csat

--- internal/models/CSAT.go
+++ internal/models/CSAT.go
@@ -1,10 +1,10 @@
 package models
 
 type CSAT struct {
-	InTotal uint8
-	Review  string
+	InTotal uint8  `json:"in_total"`
+	Review  string `json:"review"`
 }
 
 type CSATResult struct {
-	InTotalGrade float32
+	InTotalGrade float32 `json:"in_total_grade"`
 }
